174844cb5bfe180bba2e4527a29b99b5f8c29c7c
niteshtachamo <tachamonitesh@gmail.com>
2025-03-05 19:56:16 +0545

    my lab

---
+++ app/src/main/java/com/bca/mobile_programming/unit_3/SignUp.java
@@ -0,0 +1,13 @@
+package com.bca.mobile_programming.unit_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.bca.mobile_programming.R;
+
+public class SignUp extends Activity {
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.lab_1_mp);
+    }
+}
